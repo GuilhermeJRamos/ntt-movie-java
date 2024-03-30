37343c7c00a0a88fb99d53eb6533e0a4a59fd997
GuilhermeJRamos <guilhermedejesusramos23@gmail.com>
2024-03-30 12:54:14 -0300

    integração com o mysql

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+…
+UacademiaCX/src/main/java/com/guilhermeramos/nttmoviejava/NttMovieJavaApplication.java,b\a\ba781876fe874725c2dbf6c57749c8fa5441ed55
+B
+academiaCX/pom.xml,f\d\fdb9c40ca7b0c54f8e8dee60483c1aa47b319e1b
--- academiaCX/src/main/java/com/guilhermeramos/nttmoviejava/NttMovieJavaApplication.java
+++ academiaCX/src/main/java/com/guilhermeramos/nttmoviejava/NttMovieJavaApplication.java
@@ -1,9 +1,12 @@
 package com.guilhermeramos.nttmoviejava;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "NTT MOVIE", version = "1", description = "API desenvolvida para Academia NTT Data"))
 public class NttMovieJavaApplication {
 
     public static void main(String[] args) {
