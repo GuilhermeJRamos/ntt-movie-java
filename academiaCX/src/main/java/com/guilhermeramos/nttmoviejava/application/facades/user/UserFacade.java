e9970d5fe142d2be76b01c993df14b9e2ea35b90
GuilhermeJRamos <guilhermedejesusramos23@gmail.com>
2024-04-01 16:43:46 -0300

    facade de user

---
+++ academiaCX/src/main/java/com/guilhermeramos/nttmoviejava/application/facades/user/UserFacade.java
@@ -0,0 +1,42 @@
+package com.guilhermeramos.nttmoviejava.application.facades.user;
+
+import com.guilhermeramos.nttmoviejava.application.dtos.user.UserDTO;
+import com.guilhermeramos.nttmoviejava.application.entities.user.User;
+import com.guilhermeramos.nttmoviejava.application.services.user.UserInterfaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFacade {
+
+    private final UserInterfaceService userService;
+
+    @Autowired
+    public UserFacade(UserInterfaceService userService) {
+        this.userService = userService;
+    }
+
+    public ResponseEntity<UserDTO> save(UserDTO userDTO) {
+
+        User user = convertToUser(userDTO);
+        User savedUser = userService.create(user);
+        UserDTO savedUserDTO = convertToUserDTO(savedUser);
+        return ResponseEntity.ok(savedUserDTO);
+    }
+
+
+    private User convertToUser(UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.getName());
+        // Aqui você pode lidar com a conversão de outros campos, como filmes favoritos
+        return user;
+    }
+
+    private UserDTO convertToUserDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName(user.getName());
+        // Aqui você pode lidar com a conversão de outros campos, como filmes favoritos
+        return userDTO;
+    }
+}
